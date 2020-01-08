b35811eae14eabe9133e689038b0b8f00deea3cf
h834001new <59662527+h834001new@users.noreply.github.com>
2020-01-08 23:49:06 +0530

    js file

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+﻿
+var app = angular.module('myapp', []);
+app.controller('ctrl', function($scope, $http) {
+  $http.get("https://newsapi.org/v1/articles?source=the-hindu&apiKey=07bbb84ccca741e2ac4b1f1a03c07642")
+  .then(function(response) {
+      $scope.records = response.data.articles;
+      setTimeout(function(){
+        carouselInt()
+    }, 1);
+      console.log(response.data.articles)
+  });
+});
